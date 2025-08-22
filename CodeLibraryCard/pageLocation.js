cf6a0bdc67325fffb3459b1e0267f9f7815d916d
InternPedia <143799518+internpediaa@users.noreply.github.com>
2025-08-22 23:07:47 +0530

    Create pageLocation.js
    Page location added

---
+++ CodeLibraryCard/pageLocation.js
@@ -0,0 +1,34 @@
+<script>
+      document.addEventListener("DOMContentLoaded", function () {
+          if (window.location.pathname === "/p/html-css-javascript.html") {
+              // Apply the style only on this page
+              let style = document.createElement("style");
+              style.innerHTML = `
+              .container {
+      position: relative !important;
+      max-width: 100% !important;
+      width: 1145px !important;
+  }
+
+  .static_page .item-post h1.entry-title {
+  	display: none;
+  }
+
+  .post-body h3 {
+  	color: #000000 !important;
+  }
+
+  .dark .post-body p {
+      color: #0d0d0de6 !important;
+  }
+
+              .post-inner-area {
+              	background: none;
+    				border: none;
+    				border-radius: 0;
+                	padding: 0;
+                  `;
+              document.head.appendChild(style);
+          }
+      });
+</script>
